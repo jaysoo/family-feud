7c3f58f8ce57eaa36f8d8657de319c87451974c8
Jack Hsu <jackh@nulogy.com>
2014-11-13 22:52:21 -0500

    cleans up index.js

--- src/modules/app/index.js
+++ src/modules/app/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
 import angular from 'angular';
-import client from './client';
+
 import templates from '../../../tmp/templates';
+
+import QuestionStore from './game/QuestionStore';
+import ChoiceStore from './game/ChoiceStore';
+
+import client from './client';
 
 var m = angular.module('app', [
   templates.name,
   client.name
 ]);
+
+m.service('questionStore', QuestionStore);
+m.service('choiceStore', ChoiceStore);
 
 export default m;
