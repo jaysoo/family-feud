7f8468b96a3a918cb52ee7a2cc0974d1eb6642c8
Jack Hsu <jackh@nulogy.com>
2014-12-09 17:31:30 -0500

    pulls in bootstrap sass

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,24 +4,16 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var rename = require('gulp-rename');
 var csso = require('gulp-csso');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 var config = global.config;
 var release = global.release;
 
 module.exports = gulp.task('styles', function () {
-  console.log(
-        __dirname + '/../../bower_components/fontawesome/scss'
-  )
   return gulp.src(config.paths.src.styles)
     .pipe(autoprefixer('last 1 version'))
-    .pipe(sass({
-      loadPath: [
-        global.BOWER_FOLDER + '/bootstrap-sass/lib',
-        global.BOWER_FOLDER + '/fontawesome/scss'
-      ]
-    }))
+    .pipe(sass())
     .pipe(gulpif(release, rename(config.filenames.release.styles), rename(config.filenames.build.styles)))
     .pipe(gulpif(release, gulp.dest(config.paths.dest.release.styles), gulp.dest(config.paths.dest.build.styles)));
 });
