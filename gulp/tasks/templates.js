b2fb78713fcd92e0cc91daa99fff76255f6209c7
Jack Hsu <jackh@nulogy.com>
2014-12-09 17:20:06 -0500

    no template compilation

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -8,9 +8,9 @@ var minifyHTML = require('gulp-minify-html');
 
 
 module.exports = gulp.task('templates', function () {
-  return gulp.src([config.paths.src.templates, config.paths.src.templatesHTML])
-    .pipe(gulpif(release, minifyHTML({empty: true, spare: true, quotes: true})))
-    .pipe(templateCache({ standalone: true }))
-    .pipe(header('module.exports = '))
-    .pipe(gulp.dest(config.paths.src.templatesCompiled));
+  return gulp.src(config.paths.src.templates)
+    //.pipe(gulpif(release, minifyHTML({empty: true, spare: true, quotes: true})))
+    //.pipe(templateCache({ standalone: true }))
+    //.pipe(header('module.exports = '))
+    .pipe(gulp.dest(config.paths.dest.build.index));
 });
